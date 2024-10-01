1724bc68a8dce6367bf52a2216a653323741d2e9
Joseph Specht <jspecht3@illinois.edu>
2024-10-01 12:22:38 -0500

    adding radius checker

--- polygon_tally/__init__.py
+++ polygon_tally/__init__.py
@@ -1,10 +1,10 @@
 # this is run every single time
-print("Starting the import process.")
+#print("Starting the import process.")
 
 from . import zernike_like
 from .zernike_like import *
-print("Successfully Imported 'zernike_like'")
+#print("Successfully Imported 'zernike_like'")
 
 from . import aggregate
 from .aggregate import *
-print("Successfully Imported 'aggregate'")
+#print("Successfully Imported 'aggregate'")
---
+++ r-alpha.py
@@ -0,0 +1,26 @@
+from polygon_tally import *
+
+# importing data
+with open("temp-check.txt", 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        num_sides = float(row[0])
+        polygon_radius = float(row[1])
+        x = float(row[2])
+        y = float(row[3])
+
+# parameters
+r = (x**2 + y**2)**(1/2) 
+theta = np.arctan2(y, x)
+
+# creating the class
+k1 = KBasis(0, 0, num_sides, polygon_radius)
+
+r_alpha = k1.variable_radius_xy(x, y)
+rho = r / r_alpha
+
+print("\n----- Python Output (Known to be Correct) -----")
+print("r: ", r)
+print("theta: ", theta)
+print("variable radius: ", r_alpha)
+print("rho: ", rho)
